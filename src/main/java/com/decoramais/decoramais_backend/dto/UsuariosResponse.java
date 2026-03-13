da202b3197232e68733bbff894254f0aaef01fc1
Lucas Morikawa <lucasmorikawa23@gmail.com>
2026-03-13 10:53:01 -0300

    chore: Ajustanto entidade Usuario e adicionando Response

---
+++ src/main/java/com/decoramais/decoramais_backend/dto/UsuariosResponse.java
@@ -0,0 +1,10 @@
+package com.decoramais.decoramais_backend.dto;
+
+public class UsuariosResponse {
+    
+    private Long id;
+    private String nome;
+    private String email;
+    private String tipo;
+
+}
--- src/main/java/com/decoramais/decoramais_backend/entity/Usuario.java
+++ src/main/java/com/decoramais/decoramais_backend/entity/Usuario.java
@@ -1,11 +1,29 @@
 package com.decoramais.decoramais_backend.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable =false)
     private Long id;
+
+    @Column(name = "nome", nullable =false)
     private String nome;
+    
+    @Column(name = "email", nullable =false, unique =true)
     private String email;
+
+    @Column(name = "senha", nullable =false)
     private String senha;
+
+    @Column(name = "tipo", nullable = false)
     private String tipo;
     
     public Long getId() {
